b566515e1319899ab04953207bb2af4bf10d57e8
heygo <8512939@qq.com>
2026-04-01 13:03:02 +0800

    fix: canvas background pure black #09090b matching project page

--- frontend/features/storyboard/Canvas.tsx
+++ frontend/features/storyboard/Canvas.tsx
@@ -872,7 +872,7 @@ export function Canvas() {
         snapToGrid={snapToGrid}
         snapGrid={[SNAP_GRID_SIZE, SNAP_GRID_SIZE]}
         proOptions={{ hideAttribution: true }}
-        className="!bg-[#0f0f11] [&_.react-flow__viewport]:transition-transform"
+        className="!bg-[#09090b] [&_.react-flow__viewport]:transition-transform"
       >
         <Background variant={BackgroundVariant.Dots} gap={24} size={1} color="#3f3f46" />
         <MiniMap
